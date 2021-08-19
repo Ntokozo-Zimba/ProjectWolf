2f40e17c03ca76fdcb326c74000ce24facc8f3cd
Ntokozo <Ntokozoflex99@gmail.com>
2021-08-19 22:45:31 +0200

    ntokozo

---
+++ app/src/main/java/com/example/projectwolf/activityPage.kt
@@ -0,0 +1,11 @@
+package com.example.projectwolf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activityPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page)
+    }
+}
