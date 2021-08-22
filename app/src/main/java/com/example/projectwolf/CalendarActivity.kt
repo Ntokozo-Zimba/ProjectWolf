2568bd04108531425d308357bb68578b3f53d07a
Ntokozo <Ntokozoflex99@gmail.com>
2021-08-23 00:36:37 +0200

    ntokozo

---
+++ app/src/main/java/com/example/projectwolf/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectwolf
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
